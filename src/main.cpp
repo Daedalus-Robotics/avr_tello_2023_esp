8800caa6de397d676fdfb58a080d7059f33e3597
Ari Stehney <AriStehney@Aris-MBP-2.localdomain>
2023-11-26 10:37:08 -0500

    Add all updates to final firmware

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,87 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include <esp_now.h>
+#include <WiFi.h>
+
+#define CHANNEL 11
+#define SERVO_PIN 6
 
 Servo myservo;
 
-int pos = 0;
-int servoPin = 6;
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
 
-void setup()
-{
-  // Allow allocation of all timers
+// config AP SSID
+void configDeviceAP() {
+  const char *SSID = "ESP_Drop";
+  bool result = WiFi.softAP(SSID, "Slave_1_Password", CHANNEL, 0);
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+    Serial.print("AP CHANNEL "); Serial.println(WiFi.channel());
+
+    WiFi.setTxPower(WIFI_POWER_19_5dBm);
+  }
+}
+
+// callback when data is recv from Master
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+  Serial.print("Last Packet Recv Data: "); Serial.println(*data);
+
+  myservo.write(*data);
+  
+  Serial.println("");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Daedalus Tello Dropper RX");
+
+  // allow allocation of all timers
   ESP32PWM::allocateTimer(0);
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
+
+  // init servo
   myservo.setPeriodHertz(50);           // standard 50 hz servo
-  myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
+  myservo.attach(SERVO_PIN, 1000, 2000); // attaches the servo on pin 18 to the servo object
+  myservo.write(0);
+
+  //Set device in AP mode to begin with
+  WiFi.mode(WIFI_AP);
+
+  // configure device AP mode
+  configDeviceAP();
+
+  // This is the mac address of the Slave in AP Mode
+  Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info.
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
-void loop()
-{
-  myservo.write(180);
+void loop() {
+  // Chill
 }
